102470afd1b1af1e15364c9cb2226b70a2ba8dda
MimoMash <mimomashyt@gmail.com>
2025-02-25 15:31:03 +0100

    made a valid inputs test for number negative 3 for squared function

--- functionsTests.mjs
+++ functionsTests.mjs
@@ -7,6 +7,7 @@ const tests = test("Squared Function");
 // Valid Inputs
 tests.isEqual(squaredNumber(2), 4, "Squared number of 2 should be 4");
 tests.isEqual(squaredNumber(4), 16, "Squared number of 4 should be 16");
+tests.isEqual(squaredNumber(-3), 9, "Squared number of -3 should be 9");
 // Invalid Inputs
 
 // Edge Cases

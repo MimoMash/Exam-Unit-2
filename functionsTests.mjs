b43a7050e8cab54c242176048edb30a74c3437ec
MimoMash <mimomashyt@gmail.com>
2025-02-25 15:27:59 +0100

    made inputs regions, and made first test in valid inputs

--- functionsTests.mjs
+++ functionsTests.mjs
@@ -1,3 +1,11 @@
 import test from "./test.mjs";
 import squaredNumber from "./functionTasks.mjs";
 
+
+const tests = test("Squared Function");
+
+// Valid Inputs
+tests.isEqual(squaredNumber(2), 4, "Squared number of 2 should be 4");
+// Invalid Inputs
+
+// Edge Cases

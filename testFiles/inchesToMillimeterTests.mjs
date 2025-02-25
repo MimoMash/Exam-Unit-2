b4bb3af43eb8c990291bae8ce87a9af8c71e07bf
MimoMash <mimomashyt@gmail.com>
2025-02-25 16:16:35 +0100

    made valid input test of floating point number 2.5

--- testFiles/inchesToMillimeterTests.mjs
+++ testFiles/inchesToMillimeterTests.mjs
@@ -6,6 +6,7 @@ const tests = test("Inches to Millimeter Function");
 //Valid Inputs
 tests.isEqual(inchesToMillimeter(2), 50.8, "2 inches is 50.8 mm");
 tests.isEqual(inchesToMillimeter(4), 101.6, "4 inches is 101.6 mm");
+tests.isEqual(inchesToMillimeter(2.5), 63.5, "2.5 inches is 63.5 mm");
 //Invalid Inputs
 
 //Edge Cases

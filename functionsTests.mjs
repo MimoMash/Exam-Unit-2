1700189361eba0fef2a48fbe180caa3d4eded933
MimoMash <mimomashyt@gmail.com>
2025-02-25 15:33:18 +0100

    made a valid inputs test for floating point number 2.5 for squared function

--- functionsTests.mjs
+++ functionsTests.mjs
@@ -8,6 +8,7 @@ const tests = test("Squared Function");
 tests.isEqual(squaredNumber(2), 4, "Squared number of 2 should be 4");
 tests.isEqual(squaredNumber(4), 16, "Squared number of 4 should be 16");
 tests.isEqual(squaredNumber(-3), 9, "Squared number of -3 should be 9");
+tests.isEqual(squaredNumber(2.5), 6.25, "Squared number of 2.5 should be 6.25");
 // Invalid Inputs
 
 // Edge Cases

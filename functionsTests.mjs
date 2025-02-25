aeb123081842ea81bd2bdc2a3cebd416656377a5
MimoMash <mimomashyt@gmail.com>
2025-02-25 15:49:16 +0100

    made invalid test of null for squared function

--- functionsTests.mjs
+++ functionsTests.mjs
@@ -10,5 +10,5 @@ tests.isEqual(squaredNumber(4), 16, "Squared number of 4 should be 16");
 tests.isEqual(squaredNumber(-3), 9, "Squared number of -3 should be 9");
 tests.isEqual(squaredNumber(2.5), 6.25, "Squared number of 2.5 should be 6.25");
 // Invalid Inputs
-
+tests.isNotANumber(squaredNumber(null), NaN, "Squared number of '2' should be NaN");
 // Edge Cases

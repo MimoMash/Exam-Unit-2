ad8d2508718bba43b51b639500fd146fd9987697
MimoMash <mimomashyt@gmail.com>
2025-02-25 16:13:23 +0100

    made valid input test of 4 inches

--- testFiles/inchesToMillimeterTests.mjs
+++ testFiles/inchesToMillimeterTests.mjs
@@ -5,6 +5,7 @@ import { inchesToMillimeter } from "../functionTasks.mjs";
 const tests = test("Inches to Millimeter Function");
 //Valid Inputs
 tests.isEqual(inchesToMillimeter(2), 50.8, "2 inches is 50.8 mm");
+tests.isEqual(inchesToMillimeter(4), 101.6, "4 inches is 101.6 mm");
 //Invalid Inputs
 
 //Edge Cases

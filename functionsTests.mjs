de7ee6ac4aa0ff7a82f65dbee8f5cf92df4364ff
MimoMash <mimomashyt@gmail.com>
2025-02-25 15:29:30 +0100

    made valid input test of number 4 for squared function

--- functionsTests.mjs
+++ functionsTests.mjs
@@ -6,6 +6,7 @@ const tests = test("Squared Function");
 
 // Valid Inputs
 tests.isEqual(squaredNumber(2), 4, "Squared number of 2 should be 4");
+tests.isEqual(squaredNumber(4), 16, "Squared number of 4 should be 16");
 // Invalid Inputs
 
 // Edge Cases

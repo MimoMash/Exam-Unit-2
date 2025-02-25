e2d1c2c7a5eb8e3e3b2d915c053f870e77f6d9cc
MimoMash <mimomashyt@gmail.com>
2025-02-25 15:48:51 +0100

    made alterations inside makePowerFunction to return NaN appropriately

--- functionTasks.mjs
+++ functionTasks.mjs
@@ -1,5 +1,13 @@
 function makePowerFunction(exponent) {
-    return (base) => base ** exponent
+    if (typeof exponent !== "number" || isNaN(exponent)) {
+        return NaN;
+    }
+    return (base) => {
+        if (typeof base !== "number" || isNaN(base)) {
+            return NaN;
+        }
+        return base ** exponent;
+    };
 }
 
 const squaredNumber = makePowerFunction(2);

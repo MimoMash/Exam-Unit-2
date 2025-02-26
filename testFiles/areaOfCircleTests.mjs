c34647c6352ce02ee9b37c70de65b5d0f3a7acf4
MimoMash <mimomashyt@gmail.com>
2025-02-26 17:04:09 +0100

    added valid input test of 1

--- testFiles/areaOfCircleTests.mjs
+++ testFiles/areaOfCircleTests.mjs
@@ -4,7 +4,7 @@ import { areaOfCircle } from "../functionTasks.mjs";
 // Area of Circle Function
 const tests = test("Area of Circle function");
 // Valid Inputs
-
+tests.isEqual(areaOfCircle(1), 3.14, "Area of a circle with radius 1 should be 3.14");
 // Invalid Inputs
 
 // Edge Cases

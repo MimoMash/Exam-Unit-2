e65e8497140725d93db56abe9f6eefbdb04ca2e5
MimoMash <mimomashyt@gmail.com>
2025-02-26 16:15:57 +0100

    added invalid input test of null for root function

--- testFiles/rootTests.mjs
+++ testFiles/rootTests.mjs
@@ -9,4 +9,5 @@ tests.isEqual(root(9), 3, "Square root of 9 is 3");
 tests.isEqual(root(1), 1, "Square root of 1 is 1");
 // Invalid Inputs
 tests.isNotANumber(root(undefined), "Undefined should return NaN");
+tests.isNotANumber(root(null), "Null should return NaN");
 // Edge Cases

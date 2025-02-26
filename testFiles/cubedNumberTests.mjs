29dedf0c9e9bf26468dc72fa3a2e78cb3276fa83
MimoMash <mimomashyt@gmail.com>
2025-02-26 16:34:55 +0100

    added valid input test for -3 for cubed number

--- testFiles/cubedNumberTests.mjs
+++ testFiles/cubedNumberTests.mjs
@@ -6,6 +6,7 @@ const tests = test("Cubed Number Function");
 // Valid Inputs
 tests.isEqual(cubedNumber(2), 8, "Cubed number of 2 should be 8");
 tests.isEqual(cubedNumber(4), 64, "Cubed number of 4 should be 64");
+tests.isEqual(cubedNumber(-3), -27, "Cubed number of -3 should be -27");
 // Invalid Inputs
 
 // Edge Cases

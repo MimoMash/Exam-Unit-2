5631c803d3b8c82d96187b3c4fda2845fa52dec0
MimoMash <mimomashyt@gmail.com>
2025-02-26 16:34:01 +0100

    added valid input test of 2 for cubed number

--- testFiles/cubedNumberTests.mjs
+++ testFiles/cubedNumberTests.mjs
@@ -1,7 +1,10 @@
 import test from "./test.mjs";
 import { cubedNumber } from "../functionTasks.mjs";
 
+// cubedNumber Function
+const tests = test("Cubed Number Function");
 // Valid Inputs
+tests.isEqual(cubedNumber(2), 8, "Cubed number of 2 should be 8");
 
 // Invalid Inputs
 

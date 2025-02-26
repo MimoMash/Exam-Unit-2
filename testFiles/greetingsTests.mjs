29b9760738eca9a718887b437c85511ea02b5b82
MimoMash <mimomashyt@gmail.com>
2025-02-26 17:18:06 +0100

    made valid input test for Mary Jane

--- testFiles/greetingsTests.mjs
+++ testFiles/greetingsTests.mjs
@@ -5,6 +5,7 @@ import { greetings } from "../functionTasks.mjs";
 const tests = test("Greetings Function");
 // Valid Inputs
 tests.isEqual(greetings("John"), "Hello John", "Greeting 'John' should return 'Hello John'");
+tests.isEqual(greetings("Mary Jane"), "Hello Mary Jane", "Greeting 'Mary Jane' should return 'Hello Mary Jane'");
 // Invalid Inputs
 
 // Edge Cases

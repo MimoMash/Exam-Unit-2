c588e600f6cda1bca9d57e0d8cbf0e5038103df0
MimoMash <mimomashyt@gmail.com>
2025-02-26 17:16:18 +0100

    made a test file for greetings function

---
+++ testFiles/greetingsTests.mjs
@@ -0,0 +1,10 @@
+import test from "./test.mjs";
+import { greetings } from "../functionTasks.mjs";
+
+// Greetings Function
+const tests = test("Greetings Function");
+// Valid Inputs
+
+// Invalid Inputs
+
+// Edge Cases

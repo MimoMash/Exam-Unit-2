6ff37db884e3adcf0b61b691a62a00ec3a9d8a42
MimoMash <mimomashyt@gmail.com>
2025-02-26 17:21:30 +0100

    added invalid input tests for null

--- testFiles/greetingsTests.mjs
+++ testFiles/greetingsTests.mjs
@@ -8,4 +8,5 @@ tests.isEqual(greetings("John"), "Hello John", "Greeting 'John' should return 'H
 tests.isEqual(greetings("Mary Jane"), "Hello Mary Jane", "Greeting 'Mary Jane' should return 'Hello Mary Jane'");
 // Invalid Inputs
 tests.isEqual(greetings(""), "Hello!", "Empty string should return 'Hello!'");
+tests.isEqual(greetings(null), "Hello!", "Null should return 'Hello!'");
 // Edge Cases

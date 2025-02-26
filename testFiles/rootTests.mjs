db1ff6861ef91692d3fd8ea65b18a02915a0ea9b
MimoMash <mimomashyt@gmail.com>
2025-02-26 16:05:15 +0100

    made valid input test of 9 for root function

--- testFiles/rootTests.mjs
+++ testFiles/rootTests.mjs
@@ -5,6 +5,7 @@ import { root } from "../functionTasks.mjs";
 const tests = test("Root Function");
 // Valid Inputs
 tests.isEqual(root(4), 2, "Square root of 4 is 2");
+tests.isEqual(root(9), 3, "Square root of 9 is 3");
 // Invalid Inputs
 
 // Edge Cases

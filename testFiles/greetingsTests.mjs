14095d058b77fd529885989ad45bfe1ee8ad6c10
MimoMash <mimomashyt@gmail.com>
2025-02-26 17:19:18 +0100

    made invalid input test of empty string

--- testFiles/greetingsTests.mjs
+++ testFiles/greetingsTests.mjs
@@ -7,5 +7,5 @@ const tests = test("Greetings Function");
 tests.isEqual(greetings("John"), "Hello John", "Greeting 'John' should return 'Hello John'");
 tests.isEqual(greetings("Mary Jane"), "Hello Mary Jane", "Greeting 'Mary Jane' should return 'Hello Mary Jane'");
 // Invalid Inputs
-
+tests.isEqual(greetings(""), "Hello!", "Empty string should return 'Hello!'");
 // Edge Cases

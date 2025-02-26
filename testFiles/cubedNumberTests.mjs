e33bafcaa6a5025825ccabe1c0db709ab0a1a083
MimoMash <mimomashyt@gmail.com>
2025-02-26 16:35:16 +0100

    added valid input test for 2.5 for cubed number

--- testFiles/cubedNumberTests.mjs
+++ testFiles/cubedNumberTests.mjs
@@ -7,6 +7,7 @@ const tests = test("Cubed Number Function");
 tests.isEqual(cubedNumber(2), 8, "Cubed number of 2 should be 8");
 tests.isEqual(cubedNumber(4), 64, "Cubed number of 4 should be 64");
 tests.isEqual(cubedNumber(-3), -27, "Cubed number of -3 should be -27");
+tests.isEqual(cubedNumber(2.5), 15.625, "Cubed number of 2.5 should be 15.625");
 // Invalid Inputs
 
 // Edge Cases

b234926c56f884824b34d21972760e5236ccae10
MimoMash <mimomashyt@gmail.com>
2025-02-26 16:42:39 +0100

    made test file for areaOfCircle function

---
+++ testFiles/areaOfCircleTests.mjs
@@ -0,0 +1,10 @@
+import test from "./test.mjs";
+import { areaOfCircle } from "../functionTasks.mjs";
+
+// Area of Circle Function
+const tests = test("Area of Circle function");
+// Valid Inputs
+
+// Invalid Inputs
+
+// Edge Cases

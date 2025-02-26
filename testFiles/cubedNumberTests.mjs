122ff69492c511c200c791dc4fbd5a45e6127d14
MimoMash <mimomashyt@gmail.com>
2025-02-26 16:34:22 +0100

    added valid input test of 4 for cubed number

--- testFiles/cubedNumberTests.mjs
+++ testFiles/cubedNumberTests.mjs
@@ -5,7 +5,7 @@ import { cubedNumber } from "../functionTasks.mjs";
 const tests = test("Cubed Number Function");
 // Valid Inputs
 tests.isEqual(cubedNumber(2), 8, "Cubed number of 2 should be 8");
-
+tests.isEqual(cubedNumber(4), 64, "Cubed number of 4 should be 64");
 // Invalid Inputs
 
 // Edge Cases

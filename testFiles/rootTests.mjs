9612e7f4f55b23080d1fbb4fabcbfc8f0a66b087
MimoMash <mimomashyt@gmail.com>
2025-02-26 16:04:52 +0100

    made valid input test of 4 for root function

--- testFiles/rootTests.mjs
+++ testFiles/rootTests.mjs
@@ -1,8 +1,10 @@
 import test from "./test.mjs";
 import { root } from "../functionTasks.mjs";
 
+// Root Function
+const tests = test("Root Function");
 // Valid Inputs
-
+tests.isEqual(root(4), 2, "Square root of 4 is 2");
 // Invalid Inputs
 
 // Edge Cases

40b90c6d84e27a69863a80f7dbb58fb236166d28
MimoMash <mimomashyt@gmail.com>
2025-02-26 15:42:19 +0100

    added invalid input test of null for inch to mm function. Also changed so isNotANumber displays message.

--- testFiles/inchesToMillimeterTests.mjs
+++ testFiles/inchesToMillimeterTests.mjs
@@ -8,6 +8,7 @@ tests.isEqual(inchesToMillimeter(2), 50.8, "2 inches is 50.8 mm");
 tests.isEqual(inchesToMillimeter(4), 101.6, "4 inches is 101.6 mm");
 tests.isEqual(inchesToMillimeter(2.5), 63.5, "2.5 inches is 63.5 mm");
 //Invalid Inputs
-tests.isNotANumber(inchesToMillimeter("2"), NaN, "'2' inches should be NaN");
+tests.isNotANumber(inchesToMillimeter("2"), "'2' inches should be NaN");
+tests.isNotANumber(inchesToMillimeter(null), "Null should be NaN");
 
 //Edge Cases

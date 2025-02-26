633eab999e549a4e06f53b82a76774483a40a9e0
MimoMash <mimomashyt@gmail.com>
2025-02-26 15:43:09 +0100

    fixed isNotANumber tests to properly display message

--- testFiles/squaredNumberTests.mjs
+++ testFiles/squaredNumberTests.mjs
@@ -10,9 +10,9 @@ tests.isEqual(squaredNumber(4), 16, "Squared number of 4 should be 16");
 tests.isEqual(squaredNumber(-3), 9, "Squared number of -3 should be 9");
 tests.isEqual(squaredNumber(2.5), 6.25, "Squared number of 2.5 should be 6.25");
 // Invalid Inputs
-tests.isNotANumber(squaredNumber(null), NaN, "Squared number of '2' should be NaN");
-tests.isNotANumber(squaredNumber(NaN), NaN, "Squared number of NaN should be NaN");
-tests.isNotANumber(squaredNumber(undefined),NaN, "Squared number of undefined should be NaN");
+tests.isNotANumber(squaredNumber(null), "Squared number of '2' should be NaN");
+tests.isNotANumber(squaredNumber(NaN), "Squared number of NaN should be NaN");
+tests.isNotANumber(squaredNumber(undefined), "Squared number of undefined should be NaN");
 // Edge Cases
 tests.isEqual(squaredNumber(Infinity), Infinity, "Squared number of infinity should be infinity");
 tests.isEqual(squaredNumber(-Infinity), Infinity, "Squared number of -infinity should be infinity");

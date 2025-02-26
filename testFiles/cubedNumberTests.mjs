39e3da4ff78ac2835e2cdc1a6f42c8d86b71bcde
MimoMash <mimomashyt@gmail.com>
2025-02-26 16:36:04 +0100

    added invaild input test of NaN for cubed number

--- testFiles/cubedNumberTests.mjs
+++ testFiles/cubedNumberTests.mjs
@@ -10,4 +10,5 @@ tests.isEqual(cubedNumber(-3), -27, "Cubed number of -3 should be -27");
 tests.isEqual(cubedNumber(2.5), 15.625, "Cubed number of 2.5 should be 15.625");
 // Invalid Inputs
 tests.isNotANumber(cubedNumber(null), "Cubed number of null should be NaN");
+tests.isNotANumber(cubedNumber(NaN), "Cubed number of NaN should be NaN");
 // Edge Cases

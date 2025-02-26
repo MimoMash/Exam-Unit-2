cfbe2f8dd31fcd702dcdb3c96e003fa5dc9f3328
MimoMash <mimomashyt@gmail.com>
2025-02-26 16:14:41 +0100

    added valid input test of 1 for root function

--- testFiles/rootTests.mjs
+++ testFiles/rootTests.mjs
@@ -6,6 +6,7 @@ const tests = test("Root Function");
 // Valid Inputs
 tests.isEqual(root(4), 2, "Square root of 4 is 2");
 tests.isEqual(root(9), 3, "Square root of 9 is 3");
+tests.isEqual(root(1), 1, "Square root of 1 is 1");
 // Invalid Inputs
 
 // Edge Cases

c1e2e8e02728fe9158bcbd1460f5de66246d597b
MimoMash <mimomashyt@gmail.com>
2025-02-27 15:00:01 +0100

    made test file for flattenarray function

---
+++ testFiles/flattenArrayTests.mjs
@@ -0,0 +1,10 @@
+import test from "./test.mjs";
+import { flattenArray } from "../functionTasks.mjs";
+
+// Flatten Array Function
+const tests = test("Flatten Array Function");
+// Valid Inputs
+
+// Invalid Inputs
+
+// Edge Cases

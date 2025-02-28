2571f2162969ad9202cd675a5f583c4f4044f738
MimoMash <mimomashyt@gmail.com>
2025-02-28 20:09:27 +0100

    made testing file for book functions and made test for booksStartingWithThe funciton

---
+++ testFiles/booksFunctionsTests.mjs
@@ -0,0 +1,11 @@
+import { books } from "../moduleSkillSummary_assignment-main/example_files/index.mjs"
+import { booksStartingWithThe, countBooksAfter1992, countBooksBefore2004, booksByAuthorsWithT } from "../functionTasks.mjs"
+import test from "./test.mjs"
+
+// Book Functions
+const tests = test("Book Functions");
+// Valid Inputs
+console.log(booksStartingWithThe(books));
+// Invalid Inputs
+
+// Edge Cases

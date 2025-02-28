27125f8066ca54f3dca48c62d690e97108992ebf
MimoMash <mimomashyt@gmail.com>
2025-02-28 20:07:47 +0100

    fixed typos from copying another file

--- testFiles/structureStatsTests.mjs
+++ testFiles/structureStatsTests.mjs
@@ -2,8 +2,8 @@ import test from "./test.mjs";
 import { structureStats } from "../functionTasks.mjs";
 import { nodes } from "../moduleSkillSummary_assignment-main/example_files/index.mjs";
 
-// Flatten Array Function
-const tests = test("Flatten Array Function");
+// Structure Stats Function
+const tests = test("Structure Stats Function");
 // Valid Inputs
 console.log(structureStats(nodes));
 // Invalid Inputs

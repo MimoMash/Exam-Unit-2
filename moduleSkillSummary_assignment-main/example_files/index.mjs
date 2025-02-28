e3fdb97c1592a5aa2f9c3cf6a91dbf6003aba3d2
MimoMash <mimomashyt@gmail.com>
2025-02-28 19:59:27 +0100

    made export file for json files

---
+++ moduleSkillSummary_assignment-main/example_files/index.mjs
@@ -0,0 +1,6 @@
+import books from "./books.json" assert { type: "json" };
+import arrays from "./arrays.json" assert { type: "json"};
+import nodes from "./nodes.json" assert { type: "json"};
+
+
+export {books, arrays, nodes};

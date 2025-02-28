72cc196dbe442245189ebb2423a52b5548c5ab69
MimoMash <mimomashyt@gmail.com>
2025-02-28 20:05:39 +0100

    created test file for structureStats function and made first test

---
+++ testFiles/structureStatsTests.mjs
@@ -0,0 +1,11 @@
+import test from "./test.mjs";
+import { structureStats } from "../functionTasks.mjs";
+import { nodes } from "../moduleSkillSummary_assignment-main/example_files/index.mjs";
+
+// Flatten Array Function
+const tests = test("Flatten Array Function");
+// Valid Inputs
+console.log(structureStats(nodes));
+// Invalid Inputs
+
+// Edge Cases

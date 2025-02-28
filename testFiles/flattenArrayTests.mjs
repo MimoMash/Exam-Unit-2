e39839574d6396367e254c67b70f4905b0c616d2
MimoMash <mimomashyt@gmail.com>
2025-02-28 20:03:29 +0100

    imported arrays from json file and tested function

--- testFiles/flattenArrayTests.mjs
+++ testFiles/flattenArrayTests.mjs
@@ -1,10 +1,11 @@
 import test from "./test.mjs";
 import { flattenArray } from "../functionTasks.mjs";
+import { arrays } from "../moduleSkillSummary_assignment-main/example_files/index.mjs";
 
 // Flatten Array Function
 const tests = test("Flatten Array Function");
 // Valid Inputs
-
+console.log(flattenArray(arrays));
 // Invalid Inputs
 
 // Edge Cases
